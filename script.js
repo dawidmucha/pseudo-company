0be935639e75dcd846812ca3a5a9b3219e90f064
dawidmucha <dawidmucha98@gmail.com>
2019-02-12 20:20:22 +0100

    jumbo paralax + review flexbox

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('scroll', function(e) {
+  var scrolled = window.pageYOffset;
+  const background = document.querySelector('#jumbotron');
+  background.style.backgroundPositionY = (scrolled * 0.3) + 'px';
+}); //inspired by @ilonacodes
