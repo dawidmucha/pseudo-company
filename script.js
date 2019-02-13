35f6f9a22f72cd4bb25fe65d023282adc8809ba7
dawidmucha <dawidmucha98@gmail.com>
2019-02-13 15:09:19 +0100

    mobile view

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ window.addEventListener('scroll', function(e) {
   const background = document.querySelector('#jumbotron');
   background.style.backgroundPositionY = (scrolled * 0.3) + 'px';
 }); //inspired by @ilonacodes
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
